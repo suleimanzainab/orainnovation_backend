40d1c3c20196f18a170123622421f1c700dd4068
suleimanzainab <157100053+suleimanzainab@users.noreply.github.com>
2024-01-21 17:48:29 +0200

    Update settings.py

--- Ora_innovation/settings.py
+++ Ora_innovation/settings.py
@@ -101,7 +101,10 @@ REST_FRAMEWORK = {
 #     }
 # }
 DATABASES = {
-	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 
