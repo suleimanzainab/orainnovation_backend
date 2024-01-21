a1be6c84e782a3d0b63202b1cb466d85941dfb49
suleimanzainab <157100053+suleimanzainab@users.noreply.github.com>
2024-01-21 18:01:58 +0200

    Update settings.py

--- Ora_innovation/settings.py
+++ Ora_innovation/settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 
 from dotenv import load_dotenv
 from django.conf.urls.static import static
-import dj_database_url
+
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
